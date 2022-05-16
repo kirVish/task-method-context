ba3b8275360eb7233a5e602837bcbba815408fd1
kirVish <prprchvish@gmail.com>
2022-05-16 15:51:20 +0300

    METHOD-CONTEXT

--- src/MethodContext.js
+++ src/MethodContext.js
@@ -8,8 +8,15 @@ import { EventEmitter } from './EventEmitter';
  */
 export const obj = {
     count: 0,
-    subscribe() {},
-    unsubscribe() {},
+    plus() {
+        obj.count++;
+    },
+    subscribe() {
+        EventEmitter.on('click', this.plus);
+    },
+    unsubscribe() {
+        EventEmitter.off('click', this.plus);
+    },
 };
 
 /*
@@ -19,7 +26,10 @@ obj1.first(1, 2, 3);
 // Внутренний вызов должен быть равносилен obj1.second(3, 2, 1)
  */
 export const obj1 = {
-    first(...args) {},
+    first(...args) {
+        const rev = args.reverse();
+        this.second(...rev);
+    },
     second() {
         // здесь ничего писать не нужно
     },
